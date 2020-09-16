3eec71183861cb7f56b24f727c30439fa06fbd95
Flourishing <huangxiangrong827@163.com>
2020-09-16 20:01:23 +0800

    Chapter 3 code snippets first submit

---
+++ code_snippet/解析ip地址/Runner.php
@@ -0,0 +1,13 @@
+<?php
+
+class Runner
+{
+    public static function run1()
+    {
+
+        $settings = simplexml_load_file(__DIR__."/resolve.xml");
+        $manager = new AddressManager();
+        $manager->outputAddresses((string)$settings->resolvedomains);
+
+    }
+}
